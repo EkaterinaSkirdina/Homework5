081188655852d58f273eaa4a9fca7c84aba43c18
Ekaterina Skirdina <katyonok.ya17@gmail.com>
2022-10-17 11:58:36 +0300

    Task 36

---
+++ Task36/Program.cs
@@ -0,0 +1,37 @@
+﻿/* Задайте одномерный массив, заполненный случайными числами. 
+Найдите сумму элементов, стоящих на нечетных позициях.*/
+
+int[] FillArray(int size)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(size*-1,size);
+    }
+    return array;
+}
+
+void PrintArray(int[] arg)
+{
+    for (int i = 0; i < arg.Length; i++)
+    {
+        Console.Write($"{arg[i]} ");
+    }
+}
+
+int SumOddNum(int[] arr)
+{
+    int sum = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i%2==1)
+        {
+            sum+=arr[i];
+        }
+    }
+    return sum;
+}
+int[] array = FillArray(10);
+PrintArray(array);
+Console.WriteLine();
+Console.WriteLine(SumOddNum(array));
